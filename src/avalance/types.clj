36541df38b3a324bde171fe5436e5111362c710d
Zenna Tavares <zennatavares@gmail.com>
2013-02-11 00:10:34 -0500

    most of initial type system in clojure

--- src/avalance/types.clj
+++ src/avalance/types.clj
@@ -1,14 +1,55 @@
+; 1. Implement tv
+; 2. Make robust, test
+; 3. do consistency mathign in func-matchaes-args
+; 4. Write transformation 
+
 :type
 
 ; every type is a list where the first value is one of
 ; fun, list, tuple, type-var, prim
 ; the remaining values depend on the first value:
 ; fun: first value is another list where each elemenet is the argument
-;      second value is a type of return argument
-; 
+;      second value is a type of return argument 
 (def reduce-t '(fun ((fun a b) (list a))
-					 (list b)))
+                     (list b))) 
 
-(defn types-match?
-	[func value]
-	)
+(def identity-t '(fun ((tv a)) (tv a)))
+
+(defn combine_bindings
+  [binding-1 binding-2]
+  (let [types-match? (and (first binding-1) (first binding-2))
+        bindings (concat (second binding-1) (second binding-2))]
+        (list types-match bindings)))
+
+(defn func-matches-args?
+  "Is a function type-consistent with a set of arguments"
+  [func-t args-t]
+  (let [func-args-t (rest func-t) bindings [true []]]
+    (get-bindings func-args-t args-t)
+
+(defn get-bindings
+  "Get the bindings yo"
+  [func-t args-t bindings]
+  (if (empty func-args-t)
+    (same-type (first func-args-t) (first func-args-t) bindings))
+    (recur (rest func-t)
+           (rest arg-t)
+           (same-type? (first func-args-t) (first args-t) bindings))))
+
+(defn same-type?
+  [t-1 t-2 bindings]
+  ""
+  (let [t-1-class (first t-1)
+        t-2-class (first t-2)
+        failed_binding '(false, [])]
+    (cond
+      (and (= t-1-class 'tv) (= t-2-class 'tv)) bindings
+      (not= t-1-class t-2-class) failed_binding
+      (= t-1-class 'fun)
+        (let [t-1-args-ret (conj (second t1) (last t1))
+              t-2-args-ret (conj (second t2) (last t2))]
+              (get-bindings t-1-args-ret t-2-args-ret bindings))
+      (= (t-1-class) 'list)
+        (let [t-1-elem-type (second t-1)
+              t-2-elem-type (second t-2)]
+          (get-bindings t-1-elem-type t-2-elem-type bindings)))))
--- src/avalance/types.py
+++ src/avalance/types.py
@@ -1,28 +1,42 @@
-inc_t = ('fun', 
-	(('num'))
-	('num'))
-map_t = ('fun'
-	(('fun', (('tv', 'a'))
-			 ('tv', 'b'))
-	 ('list', ('tv', 'a'))) 
-	('list', ('tv', 'a')))
+identity = ['fun', [['tv', 'a']], ['tv', 'a']]
 
+inc_t = ['fun', 
+	[['num']],
+	['num']]
+
+num = ['num']
+
+#Q1. if tv, does which one is tv matter
+#Q2. is bound  to a variable which containts tv then what
+map_t = ['fun', 
+	[['fun', [['tv', 'a']],
+			 ['tv', 'b']],
+	 ['list', ['tv', 'a']]], 
+	['list', ['tv', 'a']]]
+
+list_int_t = ['list', ['num']]
 
 def is_func_matches_args(func_t, arg_ts):
 	# Assume func_t is a func and arg lengths are correct
 	func_args = func_t[1]
+
+	# Get all bindings
 	for i, arg_t in enumerate(func_args):
-		same = is_same_type(arg_t, arg_ts[i], [])
+		same = is_same_type(arg_t, arg_ts[i], [True,[]])
+		print same
+
+	# Then check for consistency
 
 # Saying whether two functions are the same type is different
 # to saying whether a type matches a particular ag
 
 # If they're both a primitive type, then return True
-
 def combine_bindings(binding_1, binding_2):
 	# If any one is false then the thing cant be type consistent can it?
+	# import ipdb
+	# ipdb.set_trace()
 	type_consistent = binding_1[0] and binding_2[0]
-	return (type_consistent, binding_1[1] + binding_2[1])
+	return [type_consistent, binding_1[1] + binding_2[1]]
 
 # If its a list
 # check first value
@@ -32,31 +46,51 @@ def is_same_type(t_1, t_2, tv_bindings):
 	t_2_typeclass = t_2[0]
 
 	# What about typevars?
-	if t_1_typeclass != t_2_typeclass:
+	if (t_1_typeclass == "tv") != (t_2_typeclass == "tv"):
+		if t_1_typeclass == "tv":
+			tv_var = t_1
+			other_var = t_2
+		else:
+			tv_var = t_2
+			other_var = t_1
+
+		# import ipdb
+		# ipdb.set_trace()
+		binding = [tv_var[1], other_var[0]]
+		return combine_bindings(tv_bindings, [True, binding])
+
+	elif (t_1_typeclass == "tv") and (t_2_typeclass == "tv"):
+		print "both tv"
+		return tv_binding
+
+	elif t_1_typeclass != t_2_typeclass:
 		print "type classes are different", t_1_typeclass, t_2_typeclass
 		return combine_bindings(failed_binding, tv_bindings)
 
 	elif t_1_typeclass == 'fun':
 		t_1_args = t_1[1]
-		t_2_args = t_1[1]
+		t_2_args = t_2[1]
 
 		# Must be the same number of arms
 		if (len(t_1_args) != len(t_2_args)):
 			print "arg_lengths are different"
 			return combine_bindings(failed_binding, tv_bindings)
-
+ 
 		new_bindings = tv_bindings
 		for i in range(len(t_1_args)):
 			new_bindings = is_same_type(t_1_args[i], t_2_args[i], new_bindings)
 
 		t_1_ret = t_1[2]
 		t_2_ret = t_2[2]
-		is_same_type(t_1_args[i], t_2_args[i], new_bindings)
+		new_bindings = is_same_type(t_1_ret, t_2_ret, new_bindings)
 		return new_bindings
 
 	elif (t_1[0] == 'list'):
 		list_type_1 = t_1[1]
 		list_type_2 = t_2[1]
 		return is_same_type(list_type_1, list_type_2, tv_bindings)
-	
-	else (t_1[0] == 'tv'):
+
+if __name__ == "__main__":
+	import ipdb
+	ipdb.set_trace()
+	print is_func_matches_args(inc_t, [num])
