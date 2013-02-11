cbaa90882901a2806d27a3c6f05079a979b9b92e
Zenna Tavares <zennatavares@gmail.com>
2013-02-11 03:47:34 -0500

    type checking works on first test examples

--- src/avalance/types.clj
+++ src/avalance/types.clj
@@ -1,55 +1,86 @@
+(ns avalance.types)
+
 ; 1. Implement tv
 ; 2. Make robust, test
 ; 3. do consistency mathign in func-matchaes-args
 ; 4. Write transformation 
-
-:type
 
 ; every type is a list where the first value is one of
 ; fun, list, tuple, type-var, prim
 ; the remaining values depend on the first value:
 ; fun: first value is another list where each elemenet is the argument
 ;      second value is a type of return argument 
-(def reduce-t '(fun ((fun a b) (list a))
-                     (list b))) 
-
-(def identity-t '(fun ((tv a)) (tv a)))
-
 (defn combine_bindings
   [binding-1 binding-2]
   (let [types-match? (and (first binding-1) (first binding-2))
-        bindings (concat (second binding-1) (second binding-2))]
-        (list types-match bindings)))
+        bindings (vec (concat (second binding-1) (second binding-2)))]
+        [types-match? bindings]))
 
-(defn func-matches-args?
-  "Is a function type-consistent with a set of arguments"
-  [func-t args-t]
-  (let [func-args-t (rest func-t) bindings [true []]]
-    (get-bindings func-args-t args-t)
+(declare same-type?)
 
 (defn get-bindings
   "Get the bindings yo"
   [func-t args-t bindings]
-  (if (empty func-args-t)
-    (same-type (first func-args-t) (first func-args-t) bindings))
+  ; Assune funcs-t and args-t are same size, should check for this
+  (if (= (count func-t) 1)
+    (same-type? (first func-t) (first args-t) bindings)
     (recur (rest func-t)
-           (rest arg-t)
-           (same-type? (first func-args-t) (first args-t) bindings))))
+           (rest args-t)
+           (same-type? (first func-t) (first args-t) bindings))))
+
+(defn func-matches-args?
+  "Is a function type-consistent with a set of arguments"
+  [func-t args-t]
+  (let [func-args-t (second func-t) bindings [true []]]
+    (get-bindings func-args-t args-t bindings)))
 
 (defn same-type?
   [t-1 t-2 bindings]
   ""
   (let [t-1-class (first t-1)
         t-2-class (first t-2)
-        failed_binding '(false, [])]
+        failed_binding [false []]]
     (cond
+      (and (= t-1-class 'tv) (not= t-2-class 'tv))
+        (combine_bindings bindings [true [(second t-1) t-2]])
+
+      (and (= t-2-class 'tv) (not= t-1-class 'tv))
+        (combine_bindings bindings [true [(second t-2) t-1]])
+
+      ; If they are both type variables then do nothing
       (and (= t-1-class 'tv) (= t-2-class 'tv)) bindings
+
+      ; If neither is tvs and their types-classes mismatch, something's wrong
       (not= t-1-class t-2-class) failed_binding
+      
+      ; If both functions, recurse with arguments and return value
       (= t-1-class 'fun)
-        (let [t-1-args-ret (conj (second t1) (last t1))
-              t-2-args-ret (conj (second t2) (last t2))]
+        (let [t-1-args-ret (conj (second t-1) (last t-1))
+              t-2-args-ret (conj (second t-2) (last t-2))]
               (get-bindings t-1-args-ret t-2-args-ret bindings))
-      (= (t-1-class) 'list)
+      
+      ; If they are both lists then let's look at the list element
+      (= t-1-class 'lst)
         (let [t-1-elem-type (second t-1)
               t-2-elem-type (second t-2)]
-          (get-bindings t-1-elem-type t-2-elem-type bindings)))))
+          (same-type? t-1-elem-type t-2-elem-type bindings)))))
+
+; (def reduce-t '(fun ((fun a b) (list a))
+;                      (list b)))
+(def map-t '(fun ((fun ((tv a)) (tv b))
+   (lst (tv a))) 
+  (lst (tv a))))
+
+(def inc-t '(fun ((num)) (num)))
+
+(def list-int-t '(lst (num)))
+
+
+(defn -main
+  []
+  (func-matches-args? map-t (list inc-t list-int-t)))
+
+(require 'avalance.types)
+(use 'clojure.tools.trace)
+(trace-ns 'avalance.types)
+(-main)
